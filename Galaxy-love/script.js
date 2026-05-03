e6fd74cf618f1f7f4376f9b806656377e021fb52
zephys04 <nemoalfonso.zz@gmail.com>
2026-05-03 10:42:08 +0800

    Update script.js

--- Galaxy-love/script.js
+++ Galaxy-love/script.js
@@ -788,7 +788,7 @@ let galaxyAudio = null;
 
 function preloadGalaxyAudio() {
   const audioSources = [
-   "https://www.youtube.com/watch?v=d4OMqGKBl6E&list=RDd4OMqGKBl6E&start_radio=1&ab_channel=ARS"
+   "song.mp3"
   ];
 
   const randomIndex = Math.floor(Math.random() * audioSources.length);
